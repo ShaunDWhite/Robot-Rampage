4a6ff2c9bc8907266678dca38d4a0ca08d87e520
Shaun White <shaun_d_white@hotmail.com>
2015-03-24 21:32:53 +1000

    Wrote Script for main title audio

---
+++ Game/Assets/Scripts/Audio/s_MainTitleTheme.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class s_MainTitleTheme : MonoBehaviour {
+
+	public AudioClip Theme;
+
+	// Use this for initialization
+	void Start () {
+		Camera.main.audio.clip = Theme;		
+		Camera.main.audio.Play();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
