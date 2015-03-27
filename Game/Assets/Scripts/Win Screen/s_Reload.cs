832661fe370f265e2d6bda0fe40051dd340181fc
Shaun White <shaun_d_white@hotmail.com>
2015-03-27 20:00:25 +1000

    Add replay and main menu buttons to win screen, changed collider size of control panels

---
+++ Game/Assets/Scripts/Win Screen/s_Reload.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class s_Reload : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown(){
+		if (gameObject.name == "Replay") {
+			Application.LoadLevel("Scene_1");
+		}
+		if (gameObject.name == "Return to menu") {
+			Application.LoadLevel("Main_Menu");
+		}
+	}
+
+}
